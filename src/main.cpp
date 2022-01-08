a85ce8d4e0d9d9dd4bd59a983c68945fad4510de
emilijastevanovic <94925659+emilijastevanovic@users.noreply.github.com>
2022-01-08 17:22:49 +0100

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <learnopengl/filesystem.h>
+#include <learnopengl/shader_m.h>
+#include <learnopengl/camera.h>
+#include <learnopengl/model.h>
+
+#include <iostream>
+#include <cstdlib>
+#include <cmath>
+
+int main(){
+  
+}
